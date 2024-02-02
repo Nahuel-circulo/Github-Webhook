94246f70b6ef7300bf84894a1ded1f3ed11b4990
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-02-02 11:18:36 -0300

    create onIssue function

--- src/services/github.service.ts
+++ src/services/github.service.ts
@@ -1,3 +1,4 @@
+import { GithubIssuePayload } from "../interfaces/github-issue.interface";
 import { GithubStarPayload } from "../interfaces/github-star.interface";
 
 export class GithubService {
@@ -14,5 +15,20 @@ export class GithubService {
     message = `User ${sender.login} ${action} star on ${repository.full_name}`
 
     return message;
+  }
+  onIssue(payload: GithubIssuePayload) {
+
+    const { issue, action } = payload;
+
+    if (action === 'opened') {
+      return `An issue was opened with this title: ${issue.title}`
+    }
+    if (action === 'closed') {
+      return `An issue was closed by: ${issue.user.login}`
+    }
+    if (action === 'reopened') {
+      return `An issue was reopened by: ${issue.user.login}`
+    }
+    return `Unhandled action for the issue event ${action}`
   }
 }
