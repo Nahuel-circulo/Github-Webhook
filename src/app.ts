e8b1b02b5a702b8e55977335ec00f20eca2772e9
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-02-02 09:45:59 -0300

    use express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,22 @@
-console.log('Hola Mundo')
+import express, { Request, Response } from 'express';
+import { envs } from './config';
+
+(()=>{
+  main()
+})()
+
+
+function main(){
+
+  const app = express();
+  
+  app.post('/api/github',(req:Request,res:Response)=>{
+    res.json('Github Endpoint')
+  })
+
+
+  app.listen(envs.PORT,()=>{
+    console.log(`App running on http://localhost:${envs.PORT}`)
+  })
+
+}
