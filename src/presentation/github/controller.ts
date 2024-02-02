39f5e70921e30daec55bc4baca5d37cf48adf8ff
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-02-02 10:46:37 -0300

    use github service on controller

--- src/presentation/github/controller.ts
+++ src/presentation/github/controller.ts
@@ -1,15 +1,35 @@
 import { Request, Response } from "express";
+import { GithubService } from "../../services/github.service";
 
 
 export class GithubController {
 
 
-  constructor() { }
+  constructor(
+    private readonly githubService = new GithubService
+  ) { }
 
 
   webhookHandler = (req: Request, res: Response) => {
-    console.log('Endpoint llamado')
-    res.json('Github Endpoint')
+
+    const githubEvent = req.header('x-github-event') ?? 'unknown'
+
+    // const signature = req.header('x-hub-signature-256') ?? 'unknown'
+    console.log({ githubEvent })
+    // console.log(signature)
+    const payload = req.body;
+    let message = ''
+    switch (githubEvent) {
+      case 'star':
+        message = this.githubService.onStar(payload)
+        break;
+      default:
+        message = `Unknown Event ${githubEvent}`
+        break;
+    }
+
+    console.log(message)
+    res.status(201).send('Accepted')
   }
 
 }
