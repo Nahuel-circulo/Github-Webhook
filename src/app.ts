c32eebfc056535859264e8e95b7399055cf307c2
Nahuel-circulo <pedroso.nahuel.dev@gmail.com>
2024-02-03 14:55:49 -0300

    add SECRET_TOKEN env

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express  from 'express';
 import { envs } from './config';
 import { GithubController } from './presentation/github/controller';
 
